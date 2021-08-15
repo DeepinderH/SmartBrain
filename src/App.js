60657fa1d8b485739a6a65380b4eda68f6110cd4
Deepinder Hehar <60627733+DeepinderH@users.noreply.github.com>
2021-08-14 23:40:46 -0600

    updates

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://blooming-inlet-17733.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
